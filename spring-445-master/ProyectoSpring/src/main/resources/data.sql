d123d92a40dc957ef034c9b7fa1e2bd12e0541ef
hooliganspy <64080343+hooliganspy@users.noreply.github.com>
2022-06-10 23:44:07 -0400

    332432

--- spring-445-master/ProyectoSpring/src/main/resources/data.sql
+++ spring-445-master/ProyectoSpring/src/main/resources/data.sql
@@ -1,32 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `spring_db`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ciudades`
---
-
-CREATE TABLE `ciudades` (
-  `id` int(11) NOT NULL,
-  `ciudad` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `ciudades`
---
-
 INSERT INTO `ciudades` (`id`, `ciudad`) VALUES
 (1, 'Luque'),
 (2, 'Asunción'),
@@ -40,18 +11,6 @@ INSERT INTO `ciudades` (`id`, `ciudad`) VALUES
 -- Estructura de tabla para la tabla `clientes`
 --
 
-CREATE TABLE `clientes` (
-  `id` int(11) NOT NULL,
-  `nombres` varchar(45) NOT NULL,
-  `apellidos` varchar(45) NOT NULL,
-  `telefono` varchar(9) NOT NULL,
-  `email` varchar(75) DEFAULT NULL,
-  `ciudades_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `clientes`
---
 
 INSERT INTO `clientes` (`id`, `nombres`, `apellidos`, `telefono`, `email`, `ciudades_id`) VALUES
 (1, 'Diana', 'Lima', '7458-2356', 'diana@gmail.com', 1),
@@ -59,50 +18,3 @@ INSERT INTO `clientes` (`id`, `nombres`, `apellidos`, `telefono`, `email`, `ciud
 (3, 'Roxana', 'López', '2451-7845', 'roxan@test.com', 5),
 (4, 'David', 'Perez', '2417-4479', 'david@gmail.com', 2);
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `ciudades`
---
-ALTER TABLE `ciudades`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id`,`ciudades_id`),
-  ADD KEY `fk_clientes_ciudades_idx` (`ciudades_id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `ciudades`
---
-ALTER TABLE `ciudades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `clientes`
---
-ALTER TABLE `clientes`
-  ADD CONSTRAINT `fk_clientes_ciudades` FOREIGN KEY (`ciudades_id`) REFERENCES `ciudades` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
